806b3f934d998d24962d07d9bf5df08a8470855a
Dipterocarpaceae <bagusalmahenzar@gmail.com>
2022-04-07 14:12:45 +0700

    Layout Page

---
+++ components/Layout/index.tsx
@@ -0,0 +1,21 @@
+import Header from "../Header";
+import Footer from "../Footer";
+import styles from "./Layout.module.css";
+import { ReactNode } from "react";
+
+interface LayoutProps {
+    children: ReactNode;
+}
+
+export default function Layout(props: LayoutProps) {
+    const { children } = props;
+    return (
+        <div className={styles.container}>
+            <Header />
+            <div className={styles.content}>
+                {children}
+            </div>
+            <Footer />
+        </div>
+    )	
+}
